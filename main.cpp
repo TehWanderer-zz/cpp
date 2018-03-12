8e78ce2c8243691142692ebb1b43239bb69c67f1
TehWanderer <37311418+TehWanderer@users.noreply.github.com>
2018-03-12 21:58:17 +0000

    A

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+
+int plus(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
 
 int main()
 {
     std::vector<double> vec1(5);
-for(int i=0; i<5; i++)
-{
-    vec1[i]=i*i;
-}
-  for(int i=0; i<5; i++)
-{
-    std::cout <<vec1[i]<<std::endl;
-} 
-   
-    int a=8;
-    std::cout <<"hello world";
+    for (int i = 0; i < 5; i++)
+    {
+        vec1[i] = i * i;
+        vec1[i] += plus(3, 5);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << vec1[i] << std::endl;
+    }
+
+    int a = 8;
+    std::cout << "hello world";
 }
