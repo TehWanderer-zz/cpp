51a9d06c31ba3c0af3a49bc0fd3af3a81c3fec02
TehWanderer <37311418+TehWanderer@users.noreply.github.com>
2018-03-12 21:32:10 +0000

    danke schon

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
 #include<iostream>
+#include<vector>
 
-kjgki 
+int main()
+{
+    std::vector<double> vec1(5);
+for(int i=0; i<5; i++)
+{
+    vec1[i]=i*i;
+}
+  for(int i=0; i<5; i++)
+{
+    std::cout <<vec1[i]<<std::endl;
+} 
+   
+    int a=8;
+    std::cout <<"hello world";
+}
