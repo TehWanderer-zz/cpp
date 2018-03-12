ecfeef118b6765a02a68069a5d18f34db5024c0a
TehWanderer <37311418+TehWanderer@users.noreply.github.com>
2018-03-12 22:16:56 +0000

    .

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+@echo on
+call "%VC140%\vcvarsall.bat" x64
 set compilerflags=/EHsc /O2
 cl.exe %compilerflags% main.cpp
